9021dbb045756e05de132cba37c04a48ea52c1ed
Remix Workspace <>
2022-10-10 02:25:17 +0100

    Added simpleStorage solidity file

---
+++ Contracts/SimpleStorage.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.8;
+
+contract SimpleStorage {
+    bool hasFavouriteNumber = true;
+    uint256 favouriteNumber = 5;
+    string favouriteNumberInText = "hdsjkgfdhs";
+    int256 favouriteInt = -5;
+    address myAdress = 0x3F698D796f806988dC5901B30c2869fdb027eF41;
+    bytes favouriteBytes = "cat";
+}
